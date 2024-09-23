7f32655eef172f1197ed7a5db20c8da9aac42d3a
Oleh Tsiolkovskiy <olehtsiolkovskiy86@gmail.com>
2024-09-23 22:10:28 +0300

    basic layout

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,7 @@
+export default function PublicHome() {
+  return (
+    <div>
+      <h1>Start Page</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Start</h1>
     </div>
   );
 }
